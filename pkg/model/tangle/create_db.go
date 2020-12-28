42381a2644008d6286b2ff38345dd9a00e9ee531
massyu <19fmi04@ms.dendai.ac.jp>
2020-12-29 04:43:46 +0900

    dbの導入

--- pkg/model/tangle/create_db.go
+++ pkg/model/tangle/create_db.go
@@ -3,6 +3,7 @@ package tangle
 import (
 	// ビルド時のみ使用する
 	"database/sql"
+	"log"
 )
 
 // DB Path(相対パスでも大丈夫かと思うが、筆者の場合、絶対パスでないと実行できなかった)
@@ -21,11 +22,13 @@ type Transaction struct {
 
 func create_db() {
 	// Open(driver,  sql 名(任意の名前))
+	log.Println("create_db1")
 	DbConnection, _ := sql.Open("sqlite3", dbPath)
+	log.Println("create_db2")
 
 	// Connection をクローズする。(defer で閉じるのが Golang の作法)
 	defer DbConnection.Close()
-
+	log.Println("create_db3")
 	// データを挿入(? には、値が入る)
 	// cmd := "INSERT INTO tsc (address, value, bundle, tag) VALUES (?, ?, ?, ?)"
 	// DbConnection.Exec(cmd, "hoge1", 1, "hoge2", "hoge3")
