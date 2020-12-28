7a3df2a18ac559027c4e6fc188c63786b5979e38
massyu <19fmi04@ms.dendai.ac.jp>
2020-12-29 04:26:25 +0900

    dbの導入

--- pkg/model/tangle/create_db.go
+++ pkg/model/tangle/create_db.go
@@ -3,7 +3,6 @@ package tangle
 import (
 	// ビルド時のみ使用する
 	"database/sql"
-	"log"
 )
 
 // DB Path(相対パスでも大丈夫かと思うが、筆者の場合、絶対パスでないと実行できなかった)
@@ -27,25 +26,7 @@ func create_db() {
 	// Connection をクローズする。(defer で閉じるのが Golang の作法)
 	defer DbConnection.Close()
 
-	// blog テーブルの作成
-	cmd := `CREATE TABLE IF NOT EXISTS transaction(
-             address STRING,    
-             value INT,
-	     bundle STRING,
-	     tag STRING)`
-
-	// cmd を実行
-	// _ -> 受け取った結果に対して何もしないので、_ にする
-	_, err := DbConnection.Exec(cmd)
-
-	// エラーハンドリング(Go だと大体このパターン)
-	if err != nil {
-		// Fatalln は便利
-		// エラーが発生した場合、以降の処理を実行しない
-		log.Fatalln(err)
-	}
-
 	// データを挿入(? には、値が入る)
-	cmd2 := "INSERT INTO tsc (address, value, bundle, tag) VALUES (?, ?, ?, ?)"
-	DbConnection.Exec(cmd2, "hoge1", 1, "hoge2", "hoge3")
+	cmd := "INSERT INTO tsc (address, value, bundle, tag) VALUES (?, ?, ?, ?)"
+	DbConnection.Exec(cmd, "hoge1", 1, "hoge2", "hoge3")
 }
