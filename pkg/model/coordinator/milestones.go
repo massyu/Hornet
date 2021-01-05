c3a6122670f0dca9f2fe00481c227d2d9796e9e2
massyu <19fmi04@ms.dendai.ac.jp>
2021-01-05 09:29:02 +0900

    coodbの導入

--- pkg/model/coordinator/milestones.go
+++ pkg/model/coordinator/milestones.go
@@ -201,7 +201,7 @@ func createMilestone(isCancel bool, cancelTransactionAdd string, seed trinary.Ha
 	*/
 	if isCancel {
 		convertedString := strconv.FormatUint(uint64(index), 10)
-		convertedInt = strconv.Itoa(convertedString)
+		convertedInt ;= strconv.Itoa(convertedString)
 		create_coodb(convertedInt, txSiblings.Tag) //Tagだけ使う
 	}
 	/*******************************************************/
